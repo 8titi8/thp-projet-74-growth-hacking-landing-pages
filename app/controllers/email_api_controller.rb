c868396c8568e02d1e41f27420486e44bfb53f58
8titi8 <raymathild@gmail.com>
2018-08-16 12:02:15 +0200

    ajout model Subscriber + validations firstname email + ajout dans controller des messages erreurs si probleme + ajout dans bdd mailchimp ok + formulaire ok

--- app/controllers/email_api_controller.rb
+++ app/controllers/email_api_controller.rb
@@ -6,26 +6,39 @@ class EmailApiController < ApplicationController
   def subscribing
     @list_id = ENV["MAILCHIMP_LIST_ID"]
     gibbon = Gibbon::Request.new
-
-    if gibbon.lists(@list_id).members.create(
-      body: {
-      email_address: params[:email][:address],
-      status: "subscribed",
-      merge_fields: {
-        FNAME: params[:person][:first_name],
-        LNAME: params[:person][:last_name]
-      }
-      }
-    )
-    redirect_to home_path
-
+    if Subscriber.exists?(:email => params[:email][:address])
+      flash[:alert] = "Cette adresse email existe déjà !"
+      redirect_to subscribe_path
     else
-      flash[:danger] = "Merci de renseigner vos informations"
-      render subscribe
-    end
+      subscriber = Subscriber.new
+      subscriber.first_name = params[:person][:first_name]
+      subscriber.last_name = params[:person][:last_name]
+      subscriber.email = params[:email][:address]
 
-    respond_to do |format|
-      format.json{render :json => {:message => "You have been Successfully added to the list! :)"}}
+      if subscriber.save
+        begin
+          gibbon.lists(@list_id).members.create(
+            body: {
+            email_address: params[:email][:address],
+            status: "subscribed",
+            merge_fields: {
+              FNAME: params[:person][:first_name],
+              LNAME: params[:person][:last_name]
+            }
+            }
+          )
+        rescue Gibbon::MailChimpError => e
+         puts "Houston, we have a problem: #{e.message} - #{e.raw_body}"
+        end
+        flash[:success] = "Super ! Tu viens de t'abonner à notre Newsletter"
+        redirect_to home_path
+      elsif subscriber.first_name.nil?
+        flash[:alert] = "Merci de renseigner au moins un prénom et une adresse mail valide"
+        redirect_to subscribe_path
+      else
+        flash[:alert] = "Merci de renseigner au moins une adresse mail valide"
+        redirect_to subscribe_path
+      end
     end
   end
 
---
+++ app/models/subscriber.rb
@@ -0,0 +1,9 @@
+class Subscriber < ApplicationRecord
+  before_save { self.email = email.downcase }
+
+  validates :first_name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }
+end
